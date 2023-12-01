2bdb9ce02a9a6ff66fcfa0087cff2a2046727721
UE2020 <ttmmiizz101@gmail.com>
2023-12-01 18:12:53 -0500

    Add debug logging

--- src/main.rs
+++ src/main.rs
@@ -183,13 +183,16 @@ fn main() -> Result<(), anyhow::Error> {
                         ),
                         _ => bail!("Invalid structure from masker"),
                     };
+                    dbg!(
+                        src_mask.kind(), src_padding_mask.kind()
+                    );
                     let logits = net.method_ts(
                         "forward",
                         &[
                             src.to_device(device),
                             tgt_input.to_device(device),
                             src_mask.to_device(device),
-                            tgt_mask.to_device(device),
+                            tgt_mask.to_kind(Kind::Bool).to_device(device),
                             src_padding_mask.shallow_clone().to_device(device),
                             tgt_padding_mask.to_device(device),
                             src_padding_mask.to_device(device),
